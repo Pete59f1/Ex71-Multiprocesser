2fe9163e9dcdc95e7d27aedc20502d59bad40e64
Peter Starup <pete59f1@edu.eal.dk>
2019-04-04 09:29:28 +0200

    Add project files.

---
+++ PipeClient/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace PipeClient
+{
+    class Client
+    {
+        private string message = string.Empty;
+        public void Run(string pipeName)
+        {
+            using (NamedPipeClientStream client = new NamedPipeClientStream(pipeName))
+            {
+                Console.WriteLine("Forsøger at forbinde..");
+                client.Connect();
+                Console.WriteLine("\nForbundet");
+                using (StreamWriter writer = new StreamWriter(client))
+                {
+                    writer.AutoFlush = true;
+                    Console.WriteLine("*** Tast EXIT når du vil stoppe ***");
+                    do
+                    {
+                        Console.WriteLine("Indtast landekode og vekselkurs (f.eks.: DKK 100) :");
+                        message = Console.ReadLine();
+                        writer.WriteLine(message);
+                        Console.WriteLine($"\n{message} sendt til server");
+                    } while (message.ToUpper() != "EXIT");
+                }
+            }
+        }
+    }
+}
---
+++ PipeServer/Server.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+
+namespace PipeServer
+{
+    class Server
+    {
+        private StreamReader pipeReader;
+        private bool running = true;
+        private string input = string.Empty;
+        private Dictionary<string, Currency> currencies = new Dictionary<string, Currency>();
+
+        public void Run(string pipeName)
+        {
+            Console.WriteLine("Venter på klient...");
+            using (NamedPipeServerStream server = new NamedPipeServerStream(pipeName))
+            {
+                //Wait for client to connect:
+                server.WaitForConnection();
+                Console.WriteLine(" - klient tilsluttet!");
+                using (pipeReader = new StreamReader(server))
+                {
+                    while (running)
+                    {
+                        Console.WriteLine("Venter på input");
+                        input = GetLineFromClient(pipeReader);
+                        running = ManageInput(input);
+                    }
+                }
+            }
+        }
+
+        private string GetLineFromClient(StreamReader reader)
+        {
+            string result = string.Empty;
+            while (reader.Peek() >= 0)
+            {
+                result += reader.ReadLine();
+            }
+            reader.DiscardBufferedData();
+            return result.ToUpper();
+        }
+        private bool ManageInput(string input)
+        {
+            if (input.Equals("EXIT"))
+            {
+                return false;
+            }
+            else
+            {
+                string[] split = input.Split(' ');
+                Currency curr = new Currency(split[0], int.Parse(split[1]));
+                currencies.Add(curr.CountryCode, curr);
+                ShowCurrencies();
+                return true;
+            }
+            //Console.WriteLine("\nInput var: " + input);
+            //return !(input.ToUpper().Equals("EXIT"));
+        }
+        private void ShowCurrencies()
+        {
+            Console.WriteLine("\nValutakurser: ");
+            foreach (Currency c in currencies.Values)
+            {
+                Console.WriteLine($"{c.CountryCode}, {c.ExchangeRate}");
+            }
+        }
+    }
+}
